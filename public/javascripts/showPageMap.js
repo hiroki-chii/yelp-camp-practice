66e4190731687af14f7ac93abe84296aa8dc85b0
hiroki <hiroki0516maeda@gmail.com>
2024-03-01 00:30:52 +0900

    クラスターマップ実装

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,9 +1,16 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v12", // style URL
+  style: "mapbox://styles/mapbox/light-v11", // style URL
   center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 10, // starting zoom
 });
 
-new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
+const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
+  `<h4>${campground.title}</h4><p>${campground.location}</p`
+);
+
+new mapboxgl.Marker()
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(popup)
+  .addTo(map);
